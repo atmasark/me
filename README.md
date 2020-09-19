f57f5e2b010760ac409df5a4c260e4916663cf24
atmasark <moroatte@gmail.com>
2020-09-19 16:31:42 +0300

    remove content, temporarily forward to gnonce.com

--- README.md
+++
@@ -1,4 +0,0 @@
-<h1 align="center">
-Atte Sarkonen
-</h1>
-<p align="center"><i>me</i> -  my personal website</p>
--- src/styles/globalStyles.ts
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components'
-
-export default createGlobalStyle`
-  body{
-    -webkit-font-smoothing: antialiased;
-    margin: 0;
-    background: #FBFBFB; 
-  }
-`
